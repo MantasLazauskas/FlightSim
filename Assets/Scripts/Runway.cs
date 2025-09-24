0626169f916923ee9e6dbec7fbe310ef22766953
vazgriz <contact@ryan-vazquez.dev>
2025-09-24 14:22:04 -0500

    Add Runway class

---
+++ Assets/Scripts/Runway.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Runway : MonoBehaviour {
+    [SerializeField]
+    float length;
+
+    public Vector3 Direction { get; private set; }
+    public Vector3 End1 { get; private set; }
+    public Vector3 End2 { get; private set; }
+
+    void Awake() {
+        var transform = GetComponent<Transform>();
+        Direction = transform.forward;
+
+        var halfLength = length / 2;
+        End1 = transform.position + Direction * halfLength;
+        End2 = transform.position - Direction * halfLength;
+    }
+
+    public Vector3 GetClosestEnd(Vector3 position) {
+        var dist1 = (End1 - position).sqrMagnitude;
+        var dist2 = (End2 - position).sqrMagnitude;
+
+        if (dist1 < dist2) {
+            return End1;
+        } else {
+            return End2;
+        }
+    }
+}
